bdc619497fb4d8f5d8d56164a30e4d2e89e790b7
Vidhya <vidhya.jayakumar@gmail.com>
2019-07-31 21:54:10 -0400

    Links to new URL

---
+++ JS/script.js
@@ -0,0 +1,6 @@
+function result(){
+var x=document.getElementById("name").value;
+if(x==="Vidhya"){
+    window.location.href="file:///Users/apple/Documents/Vidhya/PersonalManagement/vidhyasite.html"
+}
+};
