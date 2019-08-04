56c7ff39b521e1aac90469ca6843929db1777dc6
Vidhya <vidhya.jayakumar@gmail.com>
2019-08-03 23:25:12 -0400

    Created code for dbinsert

--- JS/script.js
+++ JS/script.js
@@ -4,3 +4,31 @@ if(x==="Vidhya"){
     window.location.href="file:///Users/apple/Documents/Vidhya/PersonalManagement/vidhyasite.html"
 }
 };
+var database=[{
+    username:"Vidhya",
+    passwd:"Asia101"
+},
+{
+username:"Jayakumar",
+    passwd:"India101"
+},
+{
+    username:"Sriya",
+    passwd:""
+
+},
+{
+    username:"Saahas",
+    passwd:""
+
+}
+]
+
+function logIn(user,passwd){
+    var username=prompt("Enter the username");
+    var passwd=prompt("Enter the password");
+ 
+    if(username===database[0].username && 
+        passwd===database[0].passwd){
+            window.location.href="file:///Users/apple/Documents/Vidhya/PersonalManagement/vidhyasite.html"
+        }};
